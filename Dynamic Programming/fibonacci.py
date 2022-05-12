b2a3d24dac63c01afce0eae379f69e4f6db6445d
Indu Varshini <induvarshinij2000@gmail.com>
2022-05-12 15:23:07 +0530

    Added additional comments

--- Dynamic Programming/fibonacci.py
+++ Dynamic Programming/fibonacci.py
@@ -7,21 +7,19 @@ Using a hashmap datastructure for quick access : Here -- dict
 n: as keys
 fib(n) : as values of the dict
 
+Result: Time Complexity : O(n), Space Complexity : O(n)
 """
 
 
 def fib(n, memo={}):
     # new base case to check if the value is already present in the memo
-    if n in memo:
-        return memo[n]
+    if n in memo: return memo[n]
 
     # actual base case
-    if n <= 2:
-        return 1
+    if n <= 2: return 1
 
     # if value not found in the memo, calculate and add it in the memo
     memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
-
     return memo[n]
 
 
